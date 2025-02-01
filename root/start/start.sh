2131a0d82d758bafacb1fecee608931ecd71782c
waazaa <waazaa@waazaa.fr>
2025-02-01 21:05:24 +0100

    Add supervisor manager

--- root/start/start.sh
+++ root/start/start.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Si aucun argument n'est passé, exécuter une commande par défaut (comme crond)
-if [ -z "$1" ]; then
-    exec cron -f -l 2
-else
-    # Exécuter la commande ou le service principal du conteneur
-    exec "$@"
-fi
+
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
